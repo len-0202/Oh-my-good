eb40a3a40e43db4fcebc5c072e5a917d0fc3bc29
hiiragi1203 <shuna6129@gmail.com>
2026-06-12 16:05:21 +0900

    save

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import time
+import gpiozero
 
-while Mode_button == 1:
-    Camera_judge += CAMERA() #カメラの画像認識
+SW_PIN0 = 17
+SW_PIN1 = 18
+
+sw0 = gpiozero.DigitalInputDevice(SW_PIN0, pull_up = False) #オレンジ
+sw1 = gpiozero.DigitalInputDevice(SW_PIN1, pull_up = False) #緑
+
+while True:
+    if sw0.value == 1:
+        Camera_judge += CAMERA() #カメラの画像認識
     Sleepy_judge += Sleepy(Camera_judge) #居眠りの判定
     
     if Sleepy_judge >= 1: #居眠り中か判断
@@ -14,3 +22,25 @@ while Mode_button == 1:
 
 
 while Mode_button == 0:
+
+
+import gpiozero
+import time
+
+SW_PIN = 18
+
+sw = gpiozero.DigitalInputDevice(SW_PIN, pull_up=False)
+
+started = False
+
+while True:
+    # 一度でも押されたら started を True にする
+    if sw.value == 1:
+        started = True
+
+    if started:
+        print("実行中")
+    else:
+        print("待機中")
+
+    time.sleep(0.5)
