e164da49778cc7318f777d12a6b996acebbd6acd
hiiragi1203 <shuna6129@gmail.com>
2026-06-19 15:37:41 +0900

    save

--- test_switch.py
+++ test_switch.py
@@ -9,14 +9,14 @@ sw_old = 0
 mode = False
 
 while True:
-    sw = sw.value  # ボタンの状態を取得 (0 or 1)
+    sw_new = sw.value  # ボタンの状態を取得 (0 or 1)
 
     # ボタンが押された瞬間
-    if sw == 1 and sw_old == 0:
+    if sw_new == 1 and sw_old == 0:
         mode = not mode  # モード切替
         print("ON")
 
-    sw_old = sw
+    sw_old = sw_new
 
     if mode:
         print("モードA実行中")
