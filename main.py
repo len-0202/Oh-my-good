7c46592d7b28d5277452cc99090eabb092968b25
hiiragi1203 <shuna6129@gmail.com>
2026-07-03 15:05:36 +0900

    save

--- main.py
+++ main.py
@@ -12,22 +12,25 @@ import face_detect
 #sw0 = gpiozero.DigitalInputDevice(SW_PIN0) 
 #sw1 = gpiozero.DigitalInputDevice(SW_PIN1) #緑
 
-switch = int(input())
+threading.Thread(
+    target=face_detect.CAMERA,
+    daemon=True
+).start()
 
-if switch == 1:
-    camera_status = face_detect.get_camera_status() #カメラの画像認識
-    
-    if camera_status == 1: #居眠り中か判断
-        print("居眠り中\n")
-        print("アームに信号を送信")
+while True:
 
-        #アームに信号を送信
-        time.sleep(10) #10秒待機
+    switch = int(input("1を入力: "))
 
-        #music() # 音楽再生
+    if switch == 1:
 
-    else: #Sleepy_judge == 0
-        print("居眠りしていません\n")
-else:
-    print("値が違います")
-         
+        if face_detect.get_camera_status() == 1:
+            print("居眠り中")
+            print("アームに信号を送信")
+
+            #アームに信号を送信
+            time.sleep(10) #10秒待機
+
+            #music() # 音楽再生
+
+        else:#Sleepy_judge == 0
+            print("居眠りしていません")
