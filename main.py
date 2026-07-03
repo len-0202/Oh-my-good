58e4314afc91c3ef83a681c75e3062af3316e13e
hiiragi1203 <shuna6129@gmail.com>
2026-07-03 14:49:45 +0900

    save

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import face_detect
 #sw0 = gpiozero.DigitalInputDevice(SW_PIN0) 
 #sw1 = gpiozero.DigitalInputDevice(SW_PIN1) #緑
 
-value = input("値を入力してください: ")
+switch = input("値を入力してください: ")
 
 if switch == 1:
     camera_status = face_detect.get_camera_status() #カメラの画像認識
