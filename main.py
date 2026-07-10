85433a0219957afa065ba0de8c6086c5d739b24f
hiiragi1203 <shuna6129@gmail.com>
2026-07-10 15:07:39 +0900

    save

--- main.py
+++ main.py
@@ -22,36 +22,36 @@ threading.Thread(
 ).start()
 
 while True:
+
+    # ボタンが押された瞬間
     if button.is_pressed:
         mode = not mode
 
-        print("監視開始")
+        if mode:
+            print("監視開始")
+        else:
+            print("監視終了")
 
         while button.is_pressed:
             time.sleep(0.01)
 
-        if mode:
-            if face_detect.get_camera_status() == 1:
+    if mode:
+        if face_detect.get_camera_status() == 1:
 
-                print("居眠り中")
-                print("アームに信号を送信")
+            print("居眠り中")
+            print("アームに信号を送信")
 
-                # アーム制御
-                time.sleep(10)
-                #music() # 音楽再生
-                time.sleep(10)
+            # アーム制御
+            time.sleep(10)
+            #music() # 音楽再生
+            time.sleep(10)
 
-                print("起きるまで待機...")
+            print("起きるまで待機...")
 
-                # 起きるまで待機
-                while face_detect.get_camera_status() == 1:
-                    time.sleep(0.5)
+            # 起きるまで待機
+            while face_detect.get_camera_status() == 1:
+                time.sleep(0.5)
 
-                print("監視再開")
+            print("監視再開")
 
-            time.sleep(0.5)
-
-        else:
-            print("監視終了")
-
-        time.sleep(0.05)
+    time.sleep(0.1)
